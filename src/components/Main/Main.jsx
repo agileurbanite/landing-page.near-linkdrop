9bda7f3466c1e6569e99a9da594a9967bec9c061
dzhumanrm <dzhumanrm@gmail.com>
2021-11-01 16:29:32 +0200

    Fix page width

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { TopNav } from "./TopNav/TopNav";
 import { ConnectWallet } from "./ConnectWallet/ConnectWallet";
 import { Generate } from "./Generate/Generate";
 import { Distribute } from "./Distribute/Distribute";
@@ -7,10 +8,13 @@ import { PhysDrops } from "./PhysDrops/PhysDrops";
 import { TwitterInt } from "./TwitterInt/TwitterInt";
 import { Partners } from "./Partners/Partners";
 import { Header } from "./Header/Header";
-import {Footer} from "./Footer/Footer";
-export const Main = () => {
-  return (
-    <>
+import { Footer } from "./Footer/Footer";
+import * as appStyles from "../App.module.css";
+
+export const Main = () => (
+  <>
+    <TopNav />
+    <div className={appStyles.contentContainer}>
       <Header />
       <ConnectWallet />
       <Generate />
@@ -20,6 +24,6 @@ export const Main = () => {
       <TwitterInt />
       <Partners />
       <Footer />
-    </>
-  );
-};
+    </div>
+  </>
+);
